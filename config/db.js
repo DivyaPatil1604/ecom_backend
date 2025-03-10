d0f26851dfe8b9dcb96f1f1011fec64d2481ead7
DivyaPatil1604 <divyapatil9110@gmail.com>
2025-03-10 13:05:33 +0530

    second

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 require("dotenv").config()
 const connectDB=async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/dew")
+        await mongoose.connect("mongodb+srv://divyapatil:<db_password>@cluster0.eapvo.mongodb.net/divya")
         console.log('MongoDB Connected...')
     }catch(err){
         console.error(err.message)
